1dc6e5b859f81ed36308c1b6a2947f1066539d8d
Matias Saravia <matiasaravia52@gmail.com>
2019-07-09 21:54:21 -0300

    Modificaciones

--- src/pages/Inicio.js
+++ src/pages/Inicio.js
@@ -2,21 +2,44 @@ import  React from 'react';
 
 import Parametros from '../components/Parametros'
 import Datos from '../components/Datos'
+import './inicio.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPenSquare, faCheckCircle, faStream } from '@fortawesome/free-solid-svg-icons'
+
 class Inicio extends React.Component{
+    constructor(props) {
+        super(props)
+        this.state={
+            show: 1
+        }
+    }
     render(){
         return (
             <div>
-                <h1>Hola estas en inicio</h1>
-                <div className="container">
-                    <div className="row">
-                        <div className="col-3 borde">
-                            <Parametros/>
-                        </div>
-                        <div className="col-9 borde">
-                            <Datos/>
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div class="navbar-nav row">
+                        <button class={`button-navbar col-4 ${this.state.show === 1 && 'selected'}`} onClick={() => {this.setState({show:1})}}><FontAwesomeIcon icon={faPenSquare} />      Cargar datos<span class="sr-only">(current)</span></button>
+                        <button class={`button-navbar col-4 ${this.state.show === 2 && 'selected'}`} onClick={() => {this.setState({show:2})}}><FontAwesomeIcon icon={faCheckCircle} />      Solucion</button>
+                        <button class={`button-navbar col-4 ${this.state.show === 3 && 'selected'}`} onClick={() => {this.setState({show:3})}}><FontAwesomeIcon icon={faStream} />      Interpretacion</button>
                         </div>
                     </div>
-                </div>
+                    </nav>
+                    {this.state.show === 1 &&
+                        <div>
+                            <h1>Hola estas en inicio</h1>
+                            <div className="container">
+                                <div className="row">
+                                    <div className="col-3 borde">
+                                        <Parametros/>
+                                    </div>
+                                    <div className="col-9 borde">
+                                        <Datos/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    }
             </div>
         )
     }
